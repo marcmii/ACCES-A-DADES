988fb0049a0685e5019a4ef7de3f60c066c54161
marc <2005marcmc@gmail.com>
2025-10-13 19:01:23 +0200

    update fitxers penjat

--- penjat/src/controller/Main.java
+++ penjat/src/controller/Main.java
@@ -1,34 +1,46 @@
 package controller;
 
 import java.util.Scanner;
+import view.PenjatView; // Importem la vista
 
 public class Main {
+
     public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
-		
-        view.PenjatView.mostrarMenuPrincipal();
+        Scanner in = new Scanner(System.in);
+        PenjatView view = new PenjatView();
 
-		String option = in.nextLine();
+        // Mostrem el menú
+        view.mostrarMenuPrincipal();
 
-		switch (option) {
-			case 1:
-				login();
+        // Llegim l’opció de l’usuari
+        String option = in.nextLine();
 
-				break;
-			default:
-				throw new AssertionError();
-		}
+        // Tractem l’opció
+        switch (option) {
+            case "1":
+                
+                break;
+            case "2":
+                
+                break;
+            default:
+                System.out.println("Opció no vàlida!");
+        }
 
+        in.close();
     }
-	
 
-	private static void login(){
+    private static void login(Scanner in) {
+        System.out.print("Usuari: ");
+        String usuari = in.nextLine();
 
-		String usuari = '';
-		System.out.println("Usuari: ");
-		
+        System.out.print("Contrasenya: ");
+        String password = in.nextLine();
 
-	}
+        System.out.println("Intentant fer login amb usuari: " + usuari);
+        // Aquí podries afegir la lògica d'autenticació
+    }
 
+    
 }
