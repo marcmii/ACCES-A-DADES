99d5ef8b5a792e7946bc5a561f128a33ba712ea0
marc <2005marcmc@gmail.com>
2025-11-03 19:49:40 +0100

    hjafigai

--- uf2/postgres/main.java
+++ uf2/postgres/main.java
@@ -1,31 +1,31 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
+    import java.sql.Statement;
 
-public class main {
-public static void main(String[] args) {
-
-
-String url = "jdbc:postgresql://localhost:5432/sports";
-String user = "postgres";
-String password = "1234";
-String query = "SELECT * FROM deportes";
+    public class main {
+    public static void main(String[] args) {
 
 
-try (Connection conn = DriverManager.getConnection(url, user, password);
-Statement stmt = conn.createStatement();
-ResultSet rs = stmt.executeQuery(query)) {
+    String url = "jdbc:postgresql://localhost:5432/sports";
+    String user = "postgres";
+    String password = "1234";
+    String query = "SELECT * FROM deportes";
 
-while (rs.next()) {
-int id = rs.getInt("cod");
-String nom = rs.getString("nombre");
-System.out.println("Cod: " + id + " | Nombre: " + nom);
-}
 
-} catch (SQLException e) {
-System.out.println("Error en la connexió o la consulta: " + e.getMessage());
-}
-}
-}
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+    Statement stmt = conn.createStatement();
+    ResultSet rs = stmt.executeQuery(query)) {
+
+    while (rs.next()) {
+    int id = rs.getInt("cod");
+    String nom = rs.getString("nombre");
+    System.out.println("Cod: " + id + " | Nombre: " + nom);
+    }
+
+    } catch (SQLException e) {
+    System.out.println("Error en la connexió o la consulta: " + e.getMessage());
+    }
+    }
+    }
---
+++ uf2/sportsmaven2/sports/src/main/java/dam/m6/uf2/App.java
@@ -0,0 +1,43 @@
+package dam.m6.uf2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+public static void main(String[] args) {
+
+
+String url = "jdbc:postgresql://localhost:5432/sports";
+String user = "postgres";
+String password = "1234";
+String query = "SELECT * FROM deportes";
+
+
+try (Connection conn = DriverManager.getConnection(url, user, password);
+Statement stmt = conn.createStatement();
+ResultSet rs = stmt.executeQuery(query)) {
+
+while (rs.next()) {
+int id = rs.getInt("cod");
+String nom = rs.getString("nombre");
+System.out.println("Cod: " + id + " | Nombre: " + nom);
+}
+
+} catch (SQLException e) {
+System.out.println("Error en la connexió o la consulta: " + e.getMessage());
+}
+}
+}
