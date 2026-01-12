e97b06ee6dd70d2b041aab8ade54a3ccc3fbb125
marc <2005marcmc@gmail.com>
2026-01-12 19:29:04 +0100

    intent arreglar conexio

--- uf3/projecte/src/main/java/com/example/App.java
+++ uf3/projecte/src/main/java/com/example/App.java
@@ -1,23 +1,29 @@
+package com.example;
+
 import org.bson.Document;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.set;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
 
-        String uri = "mongodb+srv://USUARI:PASSWORD@cluster.mongodb.net/?retryWrites=true&w=majority";
+        // ⚠️ CANVIA USUARI, PASSWORD i CLUSTER
+        String uri = "mongodb+srv://marc:1234@cluster0.7bfaucb.mongodb.net/?appName=Cluster0\r\n" + //
+                        "";
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
 
             MongoDatabase db = mongoClient.getDatabase("practica3");
             MongoCollection<Document> col = db.getCollection("reserves");
 
+            // INSERT
             Document novaReserva = new Document("dataReserva", new java.util.Date())
                     .append("clientNom", "Prova")
                     .append("restaurantNom", "El Racó")
@@ -25,13 +31,22 @@ public class Main {
                     .append("estat", "pendent");
 
             col.insertOne(novaReserva);
+            System.out.println("Reserva inserida");
 
+            // FIND
             col.find(eq("estat", "pendent"))
                     .forEach(doc -> System.out.println(doc.toJson()));
 
+            // UPDATE
             col.updateOne(eq("clientNom", "Prova"), set("estat", "confirmada"));
+            System.out.println("Reserva actualitzada");
 
+            // DELETE
             col.deleteOne(eq("clientNom", "Prova"));
+            System.out.println("Reserva eliminada");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
