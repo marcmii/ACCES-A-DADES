93fc88251e5659f385b40c96ddc837f3d351300b
marc <2005marcmc@gmail.com>
2026-02-09 18:27:11 +0100

    treiem .env

--- uf3/API/server.js
+++ uf3/API/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3021;
 
-// Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
